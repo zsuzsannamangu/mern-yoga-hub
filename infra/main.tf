d0f38a7215cb31f083ae1454b5c287eb156911e8
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2025-04-11 18:29:38 -0700

    modifies docker related files

--- infra/main.tf
+++ infra/main.tf
@@ -3,9 +3,20 @@ provider "aws" {
 }
 
 resource "aws_instance" "mern_app" {
-    ami           = "ami-04e914639d0cca79a"  # Ubuntu 20.04
+    ami           = "ami-0c1ade727754a7a75"
     instance_type = var.instance_type
-    user_data = file("${path.module}/setup-ssh.sh")
+    user_data = <<-EOF
+    #!/bin/bash
+    set -e
+
+    echo "Setting up authorized_keys at $(date)" >> /var/log/user-data.log
+
+    mkdir -p /home/ubuntu/.ssh
+    echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcgxwv5hz+FKMbOQnTh7x76BhBR2hW+CfwQEjR43d/AmlHSgAeWf6K4o36p0POVcdlq7ialJPv2a40A7V55nGgTnGG0r0e78ky7nhIfefDwaICXTnf7cmndw3V5EwIubQ0IX3bffgduWPC/69B2ymdWx0Y7IC1CZEN3sZ0K4hTIMw1rikkftRKCGU5HTs2X8zu/PEkmDxOGvw+6Y2Zb5B0EByiNOEEmAZ8LNLulwN87NE+L6vs81jr0JYOp9zosznOfmYQxrNw+Kj3imy0/AI55WJKenqpAHO8yGMxxR3C6djx+2mJ2hforjJTWPzDHs3L0XpIEdfh1URRORw3OG3v" > /home/ubuntu/.ssh/authorized_keys
+    chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+    chmod 700 /home/ubuntu/.ssh
+    chmod 600 /home/ubuntu/.ssh/authorized_keys
+    EOF
 
     security_groups = [aws_security_group.allow_ssh_http.name]
 
