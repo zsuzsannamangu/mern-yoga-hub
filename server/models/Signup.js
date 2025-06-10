f1367e9df32a9e1dfc721aef1ae29d0e10ebf446
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2025-06-10 12:11:48 -0700

    fixes signature image

--- server/models/Signup.js
+++ server/models/Signup.js
@@ -6,7 +6,7 @@ const signupSchema = new mongoose.Schema({
     phone: { type: String, required: true },
     classTitle: { type: String, required: true },
     date: { type: String, required: true }, // Keep as string for simplicity
-    signature: { type: String, required: true },
+    signatureUrl: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
 
