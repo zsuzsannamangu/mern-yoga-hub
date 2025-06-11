e6ecc363c8598aa4ac6e7264765273b63aee2a65
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2025-06-11 13:22:14 -0700

    chocolate page

--- client/src/components/Chocolates/AboutChocolates.js
+++ client/src/components/Chocolates/AboutChocolates.js
@@ -40,7 +40,7 @@ function AboutChocolates() {
         </motion.div>
       </div>
       <div className="about-chocolates-content-2">
-        <motion.div className="about-chocolates-image" variants={fadeInUp} initial="hidden" whileInView="visible" viewport={{ once: true }}>
+        <motion.div className="about-chocolates-image-2" variants={fadeInUp} initial="hidden" whileInView="visible" viewport={{ once: true }}>
           <img src="/images/Zsuzsi/Zsuzsi_and_chocolates_sm_2.jpg" alt="Zsuzsi" />
           <p className="photo-credit">Photo by Jason Quigley</p>
         </motion.div>
