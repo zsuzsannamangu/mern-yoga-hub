b91da31ffc1f04295d0db1f33d8c0ab1009faca7
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2025-08-01 20:16:05 -0700

    creates new paragraph in Bookings page

--- client/src/components/User/UserBookNew.js
+++ client/src/components/User/UserBookNew.js
@@ -415,7 +415,8 @@ function UserBookNew() {
                             }
                             highlightedSlots={availableSlots.map((slot) => slot.date)}
                         />
-                        <p>**Individual yoga sessions are $80-$110/hr sliding scale. Individualized yoga therapy sessions are $10-$80/hr sliding scale through June 2026, while I'm in training.</p>
+                        <p>** Individual yoga sessions are $80-$110/hr sliding scale.</p> 
+                        <p> Individualized yoga therapy sessions are $10-$80/hr sliding scale through June 2026, while I'm in training.</p>
                         <p> Your investment is a personal choice, aligning with your current financial circumstances.**</p>
                         <p><strong> Committing to at least 8 weeks of yoga therapy gives us time to build trust, personalize your practice, and support meaningful, lasting change in body, mind, and nervous system. </strong></p>
                     </div>
