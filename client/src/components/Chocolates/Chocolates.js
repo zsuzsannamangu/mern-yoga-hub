60718a5254fe254a83067094439fd29f0131693a
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2025-08-31 12:33:30 -0700

    Recommit changes without large video

--- client/src/components/Chocolates/Chocolates.js
+++ client/src/components/Chocolates/Chocolates.js
@@ -28,36 +28,6 @@ function Chocolates() {
                 <meta name="description" content="Handcrafted chocolate made with organic, fair-trade cacao or ceremonial cacao. Soy-free, vegan, palm-oil free and packaged in reusable tins." />
                 <link rel="canonical" href="https://www.yogaandchocolate.com/chocolates" />
             </Helmet>
-            {/* <motion.div
-                className='chocolates-top'
-                initial={{ opacity: 0, y: 30 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}
-            >
-                <div className="overlay">
-                    <motion.div
-                        className="overlay-text"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ delay: 0.5, duration: 1 }}
-                    >
-                        <h1>ReTreat Chocolates</h1>
-                        <p>Plant-based. Sustainably sourced. Low waste packaging.</p>
-                        <p>Organic, local and seasonal ingredients.</p>
-                        <p>I never use: gluten, soy, palm oil, and refined sugar.</p>
-                        <p>Made to order, from scratch, by hand, in very small batches.</p>
-                        <motion.button
-                            onClick={scrollToProductsSection}
-                            className="order-button"
-                            whileHover={{ scale: 1.05 }}
-                            whileTap={{ scale: 0.95 }}
-                            transition={{ type: 'spring', stiffness: 300 }}
-                        >
-                            Order
-                        </motion.button>
-                    </motion.div>
-                </div>
-            </motion.div> */}
             <motion.div
                 className="chocolates-top"
                 initial={{ opacity: 0, y: 30 }}
@@ -72,7 +42,7 @@ function Chocolates() {
                     playsInline
                     className="background-video"
                 >
-                    <source src="/images/retreatchocolatesnosound.mp4" type="video/mp4" />
+                    <source src="/videos/chocolates-bg.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
 
