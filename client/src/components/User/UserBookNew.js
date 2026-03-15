736d027ea01654119ee6809419266cea88224cf8
Zsuzsanna Mangu <zsuzsannamangu@gmail.com>
2026-03-14 22:15:32 -0700

    booking language

--- client/src/components/User/UserBookNew.js
+++ client/src/components/User/UserBookNew.js
@@ -506,7 +506,7 @@ function UserBookNew() {
     return (
         <div className="user-page">
             <div className="user-content">
-                <h3 className="section-title">Book a New Session</h3>
+                <h3 className="section-title">Book a Virtual Session</h3>
                 <div className="userbooknew-container">
                     <div className="userbooknew">
                         <header className="userbooknew-header">
@@ -655,7 +655,7 @@ function UserBookNew() {
             {/* Pricing Information - Outside main booking container */}
             <div className="pricing-info">
                 <p>** Individual yoga sessions are $80-$110/hr sliding scale.</p> 
-                <p> Individualized yoga therapy sessions: Online sessions are $10-$80/hr sliding scale, and in-person sessions in NW Portland are $20-$100/hr sliding scale through June 2026, while I'm in training.</p>
+                <p> Individualized yoga therapy sessions: Online sessions are $10-$80/hr sliding scale, and in-person sessions in NW Portland are $20-$100/hr sliding scale through June 2026, while I'm in training. Please note that online booking is only available for online sessions. For in-person sessions please contact me directly.</p>
                 <p> Your investment is a personal choice, aligning with your current financial circumstances.**</p>
                 <p><strong> Committing to at least 8 weeks of yoga therapy gives us time to build trust, personalize your practice, and support meaningful, lasting change in body, mind, and nervous system. </strong></p>
             </div>
